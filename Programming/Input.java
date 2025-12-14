6ca16b77f6494fadf670092c53559d80a4979630
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-14 23:02:27 +0530

    Scanner

--- Programming/Input.java
+++ Programming/Input.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 public class Input {
     public static void main(String[] args) {
+
+        /* */
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("give me your name");
+        String name = sc.nextLine();
+
+        System.out.print("Hi ");
+        System.out.println(name);
+
+        sc.close();
+
     }
 }
