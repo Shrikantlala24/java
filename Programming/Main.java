32777054e2759da1cf07664dfb6f45d707244f79
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-15 15:18:24 +0530

    Update Main.java

--- Programming/Main.java
+++ Programming/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
         
+        int a = 5;
+        float f = 3.322f;
+        double d = 4.2364836;
+        // short sht = 0.123s; // This line will cause a compilation error
+        long l = 123456789L;
+
+        char ch = 'A';
+        byte b = 100;
+        boolean bool = true;
     }
 }
