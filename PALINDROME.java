7b44e15424b534b7d563204163ec212798374dba
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-16 10:30:58 +0530

    Day 2

--- PALINDROME.java
+++ PALINDROME.java
@@ -1,7 +1,28 @@
+import java.util.Scanner;
+
 public class PALINDROME {
     public static void main(String[]args) {
-        String number = "12321";
-        int middle = number.length();
-        System.out.print(middle);
+        // String number = "123321";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any string: ");
+        String str = sc.next();
+
+        String rev = "";
+
+        /*  here is the logic for reversing a string
+        for(int i = str.length() - 1; i >= 0;i--) {
+            rev = rev + str.charAt(i);
+        } */
+
+        // another way to reverse a string using StringBuilder
+
+String rev = new StringBuilder().reverse()
+
+        if (rev.equals(str)){
+            System.out.println("PALINDROME");
+        } else {
+            System.out.println("NOT PALINDROME");
+        }
+
     }
 }
---
+++ STR_INPUT.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class STR_INPUT {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+    Scanner sc = new Scanner(System.in);
+    int int1 = sc.nextInt();
+    double dou = sc.nextDouble();
+    sc.nextLine();
+    String str = sc.nextLine();        
+    
+    System.out.println("String: " + str);
+    System.out.println("Double: "+ dou);   
+    System.out.println("Int: "+int1);
+    }
+}
+
+}
