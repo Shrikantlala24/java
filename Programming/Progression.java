10b27a6d41036e26c886728e0b9ea6c1b84464c6
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-16 14:26:22 +0530

    Update Progression.java

--- Programming/Progression.java
+++ Programming/Progression.java
@@ -9,11 +9,11 @@ public class Progression {
         System.out.println("First term: ");
         int a = sc.nextInt();
         
-        System.out.println("Common Ratio: ");
-        int r = sc.nextInt();
+        System.out.println("Common Difference: ");
+        int d = sc.nextInt();
         
         for(int i = 0; i <= 7; i++) {
-            System.out.print((a + (r*i)) + " ");
+            System.out.print((a + (d*i)) + " ");
             
         }
 
