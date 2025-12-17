442fd6cdd81af9376ea85ec5ff6f49187fbbe7e4
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-17 10:03:38 +0530

    Update Q1.java

--- AC lecture Codes/Variables & Datatypes/Q1.java
+++ AC lecture Codes/Variables & Datatypes/Q1.java
@@ -2,8 +2,11 @@
 
 public class Q1 {
     public static void main(String[] args) {
-        String a = "123";
-        int b = Integer.parseInt(a);
-        System.out.println(b);
+//      String a = "123";
+//      int b = Integer.parseInt(a);
+//      System.out.println(b);
+
+        
+
     }
 }
