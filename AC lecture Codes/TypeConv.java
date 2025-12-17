1c2328d9a1d943ee5e9b3f46dae26619be7a34ba
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-17 09:19:48 +0530

    lecture updates

---
+++ AC lecture Codes/TypeConv.java
@@ -0,0 +1,10 @@
+// package AC_lecture_Codes;
+import java.util.Scanner;
+
+public class TypeConv {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        
+    }
+}
