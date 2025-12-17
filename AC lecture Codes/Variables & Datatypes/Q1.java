5dba291c14fa3e2c6aa6938c0bb6d1659a0167c3
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-17 10:01:34 +0530

    Integer.parseInt

---
+++ AC lecture Codes/Variables & Datatypes/Q1.java
@@ -0,0 +1,9 @@
+// package AC lecture Codes.Variables & Datatypes;
+
+public class Q1 {
+    public static void main(String[] args) {
+        String a = "123";
+        int b = Integer.parseInt(a);
+        System.out.println(b);
+    }
+}
