6f3b245abec4f30a1b87864286f6d4f54e3c6979
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-18 10:34:48 +0530

    PALINDROME  + vowel

---
+++ Str_palindrome.java
@@ -0,0 +1,11 @@
+
+public class Str_palindrome {
+    
+    public static void main(String[] args) {
+        
+        String str = "madam";
+        
+        String rev = new StringBuilder(str).reverse().toString();
+        System.out.println((rev.equals(str)) ? "yessssss":"nooooo");
+    }
+}
---
+++ Vowel_in_str.java
@@ -0,0 +1,20 @@
+public class Vowel_in_str {
+    
+    public static void main(String[] args) {
+        String str = "hEllO nigger, motherfucker";
+        String vowel = "aeiou";
+        int vowel_count = 0;
+        
+        for(int i = 0; i < str.length(); i++) {
+            char ch = str.toLowerCase().charAt(i);
+            if(vowel.indexOf(ch) != -1) {
+                vowel_count++;
+            }
+            else {
+                continue;
+            }
+        }
+        
+        System.out.println(vowel_count);
+    }
+}
