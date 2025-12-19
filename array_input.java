d3c714c2ca6ff377edad7f3a8ce4e1db5f77cc4e
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-19 10:24:58 +0530

    Update array_input.java

--- array_input.java
+++ array_input.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class array_input {
     public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
         
+        System.out.println("Enter the number of elements: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements: ");
+
+        for(int i = 0; i < n; i++) {
+            System.out.printf("Element %d: ", i + 1);
+            arr[i] = sc.nextInt();
+        }
+
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
