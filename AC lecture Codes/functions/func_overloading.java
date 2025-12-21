c521bfc47e1ff0b40726cc7c8a6e6d8774b4a344
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-21 19:26:46 +0530

    Update func_overloading.java

--- AC lecture Codes/functions/func_overloading.java
+++ AC lecture Codes/functions/func_overloading.java
@@ -1,7 +1,13 @@
 // package AC lecture Codes.functions;
 
 public class func_overloading {
+    public static int sum(int a , int b) {
+        return a+b;
+    }
+    public static int sum (int a, int b, int c) {
+        return a+b+c;
+    }
     public static void main(String[] args) {
-        
+        System.out.printf(" sum of 2 & 4 = %d\n sum of 2,4 & 5 = %d",sum(2,4), sum(2,4,5));
     }
 }
