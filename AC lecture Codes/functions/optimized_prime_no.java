18f490a555a3a0c37d32e5c164322519b9342d74
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-21 20:06:18 +0530

    Create optimized_prime_no.java

---
+++ AC lecture Codes/functions/optimized_prime_no.java
@@ -0,0 +1,17 @@
+// package AC lecture Codes.functions;
+
+public class optimized_prime_no {
+    public static boolean isPrime(int n) {
+        boolean prime = true;
+        for(int i = 2; i*i <= n ; i++) {
+            if( n % i ==0) {
+                prime = false;
+            }
+        }
+        return prime;
+    }
+    public static void main(String[] args) {
+        int n = 29;
+        System.out.printf("%d is a prime number \nAnswer = %b", n, isPrime(n));
+    }
+}
