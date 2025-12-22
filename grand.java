6711be13f697b57c797d7e778e74f214c374343f
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-22 13:57:52 +0530

    multiple_inheritance check

---
+++ grand.java
@@ -0,0 +1,28 @@
+public class grand {
+    public static void main(String[] args) {
+        // father f1 = new father();
+        // f1.greet();
+    }
+}
+
+
+class grandfather {
+    String name;
+    void greet() {
+        System.out.println("good morning grandpa");
+    }
+}
+
+
+class grandmother {
+    String name;
+    void greet() {
+        System.out.println("good morning grandma");
+    }
+}
+
+// class father extends grandfather,grandmother {
+//     void greet() {
+//         System.out.println("good morning grandpa");
+//     }
+// }
