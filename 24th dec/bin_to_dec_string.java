db94c44d0995820ec3d269773945babcbdde7086
Shrikantlala24 <shrikantlala2023@gmail.com>
2025-12-25 14:26:46 +0530

    Update bin_to_dec_string.java

--- 24th dec/bin_to_dec_string.java
+++ 24th dec/bin_to_dec_string.java
@@ -1,7 +1,29 @@
-public class bin_to_dec_string {
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class bin_to_dec_string {
+    
+    static void btod(String n) {
+        // int num = n;
+        // int bin = 0;
+        // int i = 0 ;
+        // while(num > 0) {
+        //     int digit = num %10;
+        //     bin += digit*Math.pow(2,i);
+        //     num /= 10;
+        //     i++;
+        // }
+        int bin = 0;
+        
+        for(int i = 0; i < n.length(); i++) {
+            int num = Integer.parseInt(n.charAt(i));
+            bin += num * Math.pow(2,i);
+        }
+        System.out.println(bin);
+    }
     public static void main(String[] args) {
-        String binaryString = "1101"; // Example binary string
-        int decimalValue = Integer.parseInt(binaryString, 2);
-        System.out.println("Decimal value: " + decimalValue);
+        // int n = 1111;
+        String n = "0101";
+        btod(n);
     }
 }
