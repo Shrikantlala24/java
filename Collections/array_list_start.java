1ce24c0fb2822d17824f41f82d8eca150bbb318d
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-01-02 10:14:24 +0530

    Update array_list_start.java

--- Collections/array_list_start.java
+++ Collections/array_list_start.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 class array_list_start {
     public static void main(String[] args) {
-        
+        ArrayList<String> arr_list = new ArrayList<>();
+
+        // add the element
+        arr_list.add("apple");
+        arr_list.add("banana");
+        arr_list.add("mango");
+
+        for (String el : arr_list) {
+            System.out.println(el);
+            
+        }
+
     }
 }
