9fb8f76714792a731b347e2a8bbb1cc1adbe22e3
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-01-05 11:22:25 +0530

    Create selection_sort.java

---
+++ Sorting/selection_sort.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class selection_sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+    
+        
+        // main logic
+        
+        for(int i =0; i < n; i++) {
+            int min_i = i;
+            for(int j = i+1; j < n; j++) {
+                if(arr[j] < arr[min_i]) {
+                    min_i =j;
+                }
+            }
+            // here we'll swap the value at i to min value
+            
+            int temp = arr[i];
+            arr[i] = arr[min_i];
+            arr[min_i] = temp;
+        }
+
+        // print the sorted array
+
+        for(int i =0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
