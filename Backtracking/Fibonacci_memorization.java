8f89a4457e577d68e31020c1574f6e565e631c9a
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-01-17 11:22:23 +0530

    update

---
+++ Backtracking/Fibonacci_memorization.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Fibonacci_memorization {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+    
+        
+        int n = sc.nextInt();
+        int[] memo = new int[n + 1];
+        Arrays.fill(memo, -1);
+        int result = fibonacci(n, memo);
+        System.out.println("Fibonacci of " + n + " is: " + result);
+        sc.close();
+
+    }
+
+    public static int fibonacci(int n, int[] memo) {
+        if (n <= 1) {
+            return n;
+        }
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo);
+        return memo[n];
+    }
+}
--- Backtracking/coin_change_greedy.java
+++ Backtracking/coin_change_greedy.java
@@ -7,6 +7,14 @@ public class coin_change_greedy {
 
     }
     public static void findMinCoins(int[] coins, int amount) {
-        
+
+        Arrays.sort(coins);
+        List<Integer> result = new ArrayList<>();
+
+        for (int i = coins.length - 1; i >= 0; i--) {
+            
+            
+
+        }
     }
 }
